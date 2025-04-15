afdfd0b61011bcf7fe8645f0b4a510b46b949099
Koteswara Rao <60357623+jkotesh007@users.noreply.github.com>
2025-04-15 20:13:06 +0000

    Updated to "beginning"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.title(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.write(
+    "This is the new beginning, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
